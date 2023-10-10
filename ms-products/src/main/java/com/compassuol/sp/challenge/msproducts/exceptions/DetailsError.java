d2a505da316d78d760cdabcdb00a32f12f7322d4
Gustavo Oliveira <gustavogustgu@gmail.com>
2023-10-10 20:01:52 -0300

    Test DetailsError

---
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/exceptions/DetailsError.java
@@ -0,0 +1,17 @@
+package com.compassuol.sp.challenge.msproducts.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FieldError {
+
+    private String field;
+    private String message;
+
+}
