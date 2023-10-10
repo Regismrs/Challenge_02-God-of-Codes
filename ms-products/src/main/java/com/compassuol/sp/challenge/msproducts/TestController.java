802e7bc9ef4d80544152f4dffa3fa87dbda3c3d4
regismrs <regismrs@hotmail.com>
2023-10-10 12:33:10 -0300

    add: TestController temporario para teste de run da aplicacao

--- ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/TestController.java
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/TestController.java
@@ -1,2 +1,14 @@
-package com.compassuol.sp.challenge.msproducts;public class TestController {
+package com.compassuol.sp.challenge.msproducts;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String getTest() {
+        return "Testando controller";
+    }
+
 }
