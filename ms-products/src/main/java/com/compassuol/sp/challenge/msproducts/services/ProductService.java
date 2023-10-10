9423a0053acb1f80c50199d638002397189d2ab3
regismrs <regismrs@hotmail.com>
2023-10-10 16:57:34 -0300

    add: ProductService

---
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.compassuol.sp.challenge.msproducts.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService {
+
+    //@Autowired
+    //private ProductRepository productRepository;
+
+    public List<Object> getAll() {
+        //return List<Object> products = productRepository.findAll();
+    }
+}
