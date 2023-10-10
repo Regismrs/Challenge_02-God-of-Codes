465a0c51ae62a09a3bd5eab3717566451bfb5ff0
regismrs <regismrs@hotmail.com>
2023-10-10 16:57:49 -0300

    add: ProductController

---
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.compassuol.sp.challenge.msproducts.controllers;
+
+import com.compassuol.sp.challenge.msproducts.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Object>> getAll() {
+        List<Object> products = productService.getAll();
+        return ResponseEntity.ok(products);
+    }
+
+}
