e18fed6624c022107b04e99d8dfa91e16ffad742
regismrs <regismrs@hotmail.com>
2023-10-13 15:55:22 -0300

    test: adicionados testes para criacao de produtos com dados validos e invalidos

--- ms-products/src/test/java/com/compassuol/sp/challenge/msproducts/repositories/ProductRepositoryTest.java
+++ ms-products/src/test/java/com/compassuol/sp/challenge/msproducts/repositories/ProductRepositoryTest.java
@@ -2,6 +2,7 @@ package com.compassuol.sp.challenge.msproducts.repositories;
 
 import com.compassuol.sp.challenge.msproducts.models.entities.Product;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -13,6 +14,7 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
+import static com.compassuol.sp.challenge.msproducts.commom.ProductConstants.*;
 import static org.assertj.core.api.Assertions.*;
 
 @ActiveProfiles("test")
@@ -27,6 +29,16 @@ public class ProductRepositoryTest {
 
     @Autowired
     private TestEntityManager testEntityManager;
+
+    @AfterEach
+    public void afterEach() {
+        /**
+         *  Nos testes de persistencia o PRODUCT acaba
+         *  ganhando um id, assim garantimos que ele
+         *  sempre tera o id null apos cada teste
+         */
+        PRODUCT.setId(null);
+    }
 
     @Sql(scripts = "/sql/import_products.sql")
     @Test
@@ -45,6 +57,40 @@ public class ProductRepositoryTest {
 
         List<Product> sut = productRepository.findAll();
         assertThat(sut).isEmpty();
+    }
+
+    @Test
+    void createProductWithValidDataReturnProduct() {
+        Product product = productRepository.save(PRODUCT);
+        Product sut = testEntityManager.find(Product.class, product.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut)
+                .isEqualToComparingFieldByFieldRecursively(product);
+    }
+
+    @Test
+    public void createProductWithInvalidDataNullThrowsException() {
+        Product emptyProduct = new Product();
+        assertThatThrownBy(()->productRepository.save(emptyProduct)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void createProductWithInvalidDataEmptyThrowsException() {
+        assertThatThrownBy(
+                ()->productRepository.save(EMPTY_PRODUCT)
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void createProductWithExistentNameThrowsException() {
+        Product product = testEntityManager.persistFlushFind(PRODUCT);
+        testEntityManager.detach(product);
+        product.setId(null);
+
+        assertThatThrownBy(
+                ()-> productRepository.save(product)
+        ).isInstanceOf(RuntimeException.class);
     }
 
 }
