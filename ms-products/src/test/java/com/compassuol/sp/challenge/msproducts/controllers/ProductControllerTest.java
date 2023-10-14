2b11342cd90231c59f2e389c06134c6612db3c67
Fabio Martins <fabio.filho0820@gmail.com>
2023-10-14 18:48:16 -0300

    refactor: packages name refactor: validations changed from dto to entity

--- ms-products/src/test/java/com/compassuol/sp/challenge/msproducts/controllers/ProductControllerTest.java
+++ ms-products/src/test/java/com/compassuol/sp/challenge/msproducts/controllers/ProductControllerTest.java
@@ -1,7 +1,6 @@
 package com.compassuol.sp.challenge.msproducts.controllers;
 
-import com.compassuol.sp.challenge.msproducts.models.dtos.ProductResponseDto;
-import com.compassuol.sp.challenge.msproducts.models.entities.Product;
+import com.compassuol.sp.challenge.msproducts.domain.dto.ProductResponseDTO;
 import com.compassuol.sp.challenge.msproducts.services.ProductService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
@@ -17,7 +15,6 @@ import java.util.List;
 
 import static com.compassuol.sp.challenge.msproducts.commom.ProductConstants.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,7 +28,7 @@ class ProductControllerTest {
     void getAllProductsReturnProductsList() {
         when(productService.getAll()).thenReturn(PRODUCT_LIST);
 
-        ResponseEntity<List<ProductResponseDto>> sut = productController.getAll();
+        ResponseEntity<List<ProductResponseDTO>> sut = productController.getAll();
 
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(sut.getBody()).hasSize(2);
@@ -42,7 +39,7 @@ class ProductControllerTest {
     void getAllProductsReturnEmptyList() {
         when(productService.getAll()).thenReturn(Collections.emptyList());
 
-        ResponseEntity<List<ProductResponseDto>> sut = productController.getAll();
+        ResponseEntity<List<ProductResponseDTO>> sut = productController.getAll();
 
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(sut.getBody()).hasSize(0);
@@ -52,7 +49,7 @@ class ProductControllerTest {
     void createProductWithValidDataReturnProduct() {
         when(productService.saveProduct(PRODUCT_REQ_DTO)).thenReturn(PRODUCT_RES_DTO1);
 
-        ResponseEntity<ProductResponseDto> sut = productController.createProduct(PRODUCT_REQ_DTO);
+        ResponseEntity<ProductResponseDTO> sut = productController.createProduct(PRODUCT_REQ_DTO);
 
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(sut.getBody()).isEqualTo(PRODUCT_RES_DTO1);
