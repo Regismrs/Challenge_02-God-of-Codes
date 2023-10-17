00ebfad2a04de914f0dfac3bb6c5da92aa69253a
regismrs <regismrs@hotmail.com>
2023-10-17 12:24:05 -0300

    add: inicial post implements

--- ms-orders/src/main/java/com/compassuol/sp/challenge/msorders/mapper/OrderMapper.java
+++ ms-orders/src/main/java/com/compassuol/sp/challenge/msorders/mapper/OrderMapper.java
@@ -6,14 +6,28 @@ import com.compassuol.sp.challenge.msorders.domain.entities.OrderProduct;
 import com.compassuol.sp.challenge.msorders.domain.entities.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class OrderMapper {
 
-    public OrderResponseDto orderResponseDto(Order order, AddressViaCepDto addressViaCepDto){
-        //foreach de produto
+    public static Order toModel(OrderRequestDto orderDto){
 
+        List<OrderProduct> products =
+                orderDto.getOrderProducts().stream()
+                        .map(p -> new OrderProduct(p.getProductId(), p.getQuantity())).toList();
+
+        Order order = new Order();
+        order.setProducts(products);
+        order.setNumber(orderDto.getAddressRequestDto().getNumber());
+        order.setPostalCode(orderDto.getAddressRequestDto().getPostalCode());
+        order.setPaymentMethod(orderDto.getPaymentMethod());
+        return order;
+    }
+
+    //metodo funcionando igual ao ms-products
+    public static OrderResponseDto toDto(Order order, AddressViaCepDto addressViaCepDto){
         var addressResponseMapper = new AddressResponseDto()
                 .builder()
                 .street(addressViaCepDto.logradouro())
@@ -27,17 +41,26 @@ public class OrderMapper {
         return new OrderResponseDto()
                 .builder()
                 .id(order.getId())
+                .products(order.getProducts())
                 .addressResponseDto(addressResponseMapper)
+                .paymentMethod(order.getPaymentMethod())
+                .subtotalValue(order.getSubTotalValue())
+                .discount(order.getDiscount())
+                .totalValue(order.getTotalValue())
+                .createdDate(order.getCreatedDate())
+                .status(order.getStatus())
                 .build();
     }
 
 
+    //rever
     public OrderResponseDto orderList(Order order){
         var orderResponseDtoList = order.getProducts().stream()
                 .map(this::orderResponseDto1).collect(Collectors.toList());
         return (OrderResponseDto) orderResponseDtoList;
     }
 
+    //rever
     private OrderResponseDto orderResponseDto1(OrderProduct productResponseDto){
 
         var orderResponse = new OrderResponseDto();
