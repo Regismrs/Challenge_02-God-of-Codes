f1c20d15bdce2504154e7bc8dc4f1221b92c14a4
Gustavo Oliveira <gustavogustgu@gmail.com>
2023-10-18 15:19:03 -0300

    feature: get method in service and controller

--- ms-feedback/src/main/java/com/compassuol/sp/challenge/msfeedback/services/FeedbackService.java
+++ ms-feedback/src/main/java/com/compassuol/sp/challenge/msfeedback/services/FeedbackService.java
@@ -1,7 +1,12 @@
 package com.compassuol.sp.challenge.msfeedback.services;
 
+import com.compassuol.sp.challenge.msfeedback.domain.dto.FeedbackResponse;
+import com.compassuol.sp.challenge.msfeedback.domain.entities.Feedback;
+import com.compassuol.sp.challenge.msfeedback.mapper.FeedbackMapper;
 import com.compassuol.sp.challenge.msfeedback.repositories.FeedbackRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FeedbackService {
@@ -11,4 +16,10 @@ public class FeedbackService {
     public FeedbackService(FeedbackRepository feedbackRepository) {
         this.feedbackRepository = feedbackRepository;
     }
+    public List<FeedbackResponse> getAll(){
+        List<Feedback> feedbackList = feedbackRepository.findAll();
+
+        return FeedbackMapper.toList(feedbackList);
+    }
+
 }
